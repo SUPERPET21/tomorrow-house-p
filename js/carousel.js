f81ad680402ea65e245e0d446bad147016b66510
JunYoung <twinjyjh2@gmail.com>
2021-10-25 20:45:01 +0900

    Apply tiny-slider and style carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,13 +1,11 @@
 const slider = tns({
 	container: '.product-show .slider-list',
-	items: 1,
-	slideBy: 'page',
-	autoplay: true,
+	autoplay: false,
 	mouseDrag: true,
 	controls: false,
 	autoplayHoverPause: true,
 	autoplayButtonOutput: false,
-	navContainer: '.product-show .thumbnail',
+	navContainer: '.product-show .thumbnail .thumbnail-list',
 	navAsThumbnails: true,
 	preventScrollOnTouch: true,
 })
