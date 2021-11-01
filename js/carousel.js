a189da529cf9cd236f45ecfa08af2959e52c6da0
JunYoung <twinjyjh2@gmail.com>
2021-11-01 10:32:22 +0900

    Implement product-user-gallery of mobile and tablet/desktop device

--- js/carousel.js
+++ js/carousel.js
@@ -9,3 +9,31 @@ const slider = tns({
 	navAsThumbnails: true,
 	preventScrollOnTouch: true,
 })
+
+const userGallerySliderIsMobile = tns({
+	container: '.product-user-gallery.is-mobile .slider-list',
+	autoplay: false,
+	mouseDrag: true,
+	controls: false,
+	autoplayHoverPause: true,
+	autoplayButtonOutput: false,
+	navContainer: '.product-user-gallery.is-mobile .thumbnail .thumbnail-list',
+	navAsThumbnails: true,
+	preventScrollOnTouch: true,
+	edgePadding: 20,
+	gutter: 4,
+	loop: false,
+})
+
+const userGallerySliderIsTablet = tns({
+	container: '.product-user-gallery.is-tablet .slider-list',
+	mouseDrag: true,
+	navContainer: '.product-user-gallery.is-tablet .thumbnail .thumbnail-list',
+	navAsThumbnails: true,
+	preventScrollOnTouch: true,
+	edgePadding: 75,
+	gutter: 6,
+	loop: false,
+	controls: true,
+	controlsContainer: '.product-user-gallery.is-tablet .arrow-button-group',
+})
